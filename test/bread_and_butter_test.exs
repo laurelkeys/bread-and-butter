eb229ec960d4a436caa41857303015d5263dec7e
Tiago Loureiro Chaves <tiago.chaves@movile.com>
2016-10-19 22:09:29 -0200

    Test if basic sum and multiply functions are working properly

--- test/bread_and_butter_test.exs
+++ test/bread_and_butter_test.exs
@@ -5,4 +5,16 @@ defmodule BreadAndButterTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "times3(num) multiplies num by 3" do
+    assert BreadAndButter.times3(5) == 15
+  end
+
+  test "plus5(num) adds 5 to num" do
+    assert BreadAndButter.plus5(5) == 10
+  end
+
+  test "times3thenPlus5(num) is equal to [(3*num)+5]" do
+    assert BreadAndButter.times3thenPlus5(5) == 20
+  end
 end
